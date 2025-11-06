5e69fcf583ea1083f8cb40a1d7f566d6e2dfdb29
lucasamorimcruz-a11y <lucas.amorim.cruz@gmail.com>
2025-11-06 04:38:17 -0300

    Add files via upload

--- buddy_alloc.c
+++ buddy_alloc.c
@@ -5,11 +5,10 @@
 #include <unistd.h>
 #include <syscall.h>
 #include <sys/mman.h>
-#define MIN 6       // 2⁶ = 64 bytes -- metadados tem 32, logo não pode ser 32(size_t = 8, bool 8, mb 8)
+#define MIN_SIZE 6  // 2⁶ = 64 bytes -- metadados tem 32, logo não pode ser 32(size_t = 8, bool 8, mb 8)
 #define MAX_SIZE 32 // 2³² = 4GB
-#define NUM_OF_SIZES (MAX_ORDER - MIN_ORDER + 1)
-
-static memory_block *[NUM_ORDERS];
+#define NUM_OF_SIZES (MAX_SIZE - MIN_SIZE + 1)
+#define HEAP_SIZE NUM_OF_SIZES 
 typedef struct memory_block
 {
     size_t size;
@@ -18,13 +17,17 @@ typedef struct memory_block
     memory_block *next;
 } memory_block;
 typedef struct memory_block memory_block;
+static memory_block[NUM_OF_SIZES];
 
 void *heap_initializer(size_t heap_size)
 {
     void *start_of_heap = mmap(
         NULL, // quando NULL, o kernel vai escolher o endereço page aligned. alinhamento de memória e paging são conceitop
         heap_size,
-
+        PROT_READ | PROT_WRITE,
+        MAP_PRIVATE | MAP_ANONYMOUS,
+        -1,
+        0
     );
     return start_of_heap;
 }
@@ -47,9 +50,6 @@ void *split(memory_block *block, size_t target)
 {
     while (block->size >= target)
     {
-        size_t half_size = block->size / 2;
-
-        memory_block *buddy = (memory_block *)((char *)block + target);
     }
 }
 void *merge()
