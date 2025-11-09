ed71edc9a78d19c86988f0981c368fbf9f11348e
lucasamorimcruz-a11y <lucas.amorim.cruz@gmail.com>
2025-11-09 00:28:12 -0300

    Add files via upload
    fixing and refactoring

--- buddy_alloc.c
+++ buddy_alloc.c
@@ -1,14 +1,15 @@
 #include <stddef.h>
 #include <stdbool.h>
-#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <syscall.h>
 #include <sys/mman.h>
+
 #define MIN_SIZE 6  // 2⁶ = 64 bytes -- metadados tem 32, logo não pode ser 32(size_t = 8, bool 8, mb 8)
 #define MAX_SIZE 32 // 2³² = 4GB
 #define NUM_OF_SIZES (MAX_SIZE - MIN_SIZE + 1)
-#define HEAP_SIZE NUM_OF_SIZES 
+#define HEAP_SIZE NUM_OF_SIZES
+
 typedef struct memory_block
 {
     size_t size;
@@ -17,20 +18,20 @@ typedef struct memory_block
     memory_block *next;
 } memory_block;
 typedef struct memory_block memory_block;
-static memory_block[NUM_OF_SIZES];
 
-void *heap_initializer(size_t heap_size)
-{
-    void *start_of_heap = mmap(
-        NULL, // quando NULL, o kernel vai escolher o endereço page aligned. alinhamento de memória e paging são conceitop
-        heap_size,
-        PROT_READ | PROT_WRITE,
-        MAP_PRIVATE | MAP_ANONYMOUS,
-        -1,
-        0
-    );
-    return start_of_heap;
-}
+bool is_power_of_2(size_t number);
+size_t round_up_to_power_of_2(size_t size);
+int search_for_list_index(size_t size, memory_block *block);
+void *buddy_split(memory_block *block, size_t target);
+void *buddy_merge(memory_block *buddy);
+bool check_if_block_free(memory_block *block);
+void *my_buddy_malloc(size_t size);
+void *my_buddy_realloc(void *ptr_of_mem_to_be_realloc, size_t size);
+void *my_buddy_calloc(size_t new_memory_blocks, size_t size);
+void *my_buddy_free(memory_block *buddy_to_be_freed);
+void *heap_initializer(size_t heap_size);
+
+memory_block *free_lists[NUM_OF_SIZES];
 
 bool is_power_of_2(size_t number)
 {
@@ -45,41 +46,67 @@ size_t round_up_to_power_of_2(size_t size)
     }
     return i;
 }
-
-void *split(memory_block *block, size_t target)
+int search_for_list_index(size_t size, memory_block *block)
+{
+    for (int i = 0; i < NUM_OF_SIZES; i++)
+    {
+        if (size == free_lists[i]->size)
+        {
+            return i;
+        }
+    }
+    printf("Não foi possível encontrar espaço para o processo");
+    return -1;
+}
+void *buddy_split(memory_block *block, size_t target)
 {
     while (block->size >= target)
     {
+        memory_block *buddy_block = (memory_block *)(char *)target + block->size;
+        split(buddy_block, target);
     }
 }
-void *merge()
+void *buddy_merge(memory_block *buddy)
 {
+    // xor pra encontrar o buddy do buddy somente quando ele estiver disponível
+    if (buddy->is_free == true &&)
 }
 bool check_if_block_free(memory_block *block)
 {
     return block->is_free;
 }
-
 void *my_buddy_malloc(size_t size)
 {
     if (!is_power_of_2(size))
     {
         size = round_up_to_power_of_2(size);
     }
+    int i = search_for_list_index(size);
 }
-void *my_buddy_free()
+void *my_buddy_realloc(void *ptr_of_mem_to_be_realloc, size_t size)
 {
 }
-void *my_buddy_realloc()
+void *my_buddy_calloc(size_t new_memory_blocks, size_t size)
 {
 }
-void *my_buddy_calloc()
+void *my_buddy_free(memory_block *buddy_to_be_freed)
 {
+    if (is_buddy_free())
+    {
+        buddy_merge();
+    }
 }
-void *gc_collect()
+void *heap_initializer(size_t heap_size)
 {
+    void *start_of_heap = mmap(
+        NULL, // quando NULL, o kernel vai escolher o endereço page aligned. alinhamento de memória e paging são conceitop
+        heap_size,
+        PROT_READ | PROT_WRITE,
+        MAP_PRIVATE | MAP_ANONYMOUS,
+        -1,
+        0);
+    return start_of_heap;
 }
-
 int main()
 {
 }
