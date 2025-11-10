aa25ad7fec66c53055003d8baf0c2b5b2110d16f
lucasamorimcruz-a11y <lucas.amorim.cruz@gmail.com>
2025-11-09 22:48:47 -0300

    Add files via upload

--- buddy_allocator.c
+++ buddy_allocator.c
@@ -32,21 +32,46 @@ void *buddy_calloc(size_t number_of_elements, size_t size);
 void *buddy_realloc(void *ptr, size_t size);
 void *buddy_free(void *ptr);
 
-/*testes do código*/
+/*Testes do código*/
 int main()
 {
-    int *ptr = (int*)buddy_alloc(sizeof(int));
+    int *ptr = (int *)buddy_alloc(sizeof(int));
     *ptr = 10;
     printf("%i", *ptr);
-    int *second_ptr = (int *)buddy_calloc(5,sizeof(int));
-    for(int *i = second_ptr; i < second_ptr + 4; i++){
+    int *second_ptr = (int *)buddy_calloc(5, sizeof(int));
+    for (int *i = second_ptr; i < second_ptr + 4; i++)
+    {
         printf("%i", *i);
     }
-    for(int i = 0; i < 4; i++){
-        second_ptr[i] = i+10;
+    for (int i = 0; i < 4; i++)
+    {
+        second_ptr[i] = i + 10;
         printf("%i", second_ptr[i]);
     }
     buddy_realloc((int *)ptr, 3);
     buddy_free(ptr);
     buddy_free(second_ptr);
+}
+/*Implementação das funções auxiliares*/
+bool is_power_of_2(size_t size)
+{
+    return ((size & (size - 1)) == 0); // exemplo de como funciona: 8 -> 1000. 7 -> 0111. 0111 & 1000 -> 0000 -> 0, logo, verdadeiro
+}
+size_t round_up_to_power_of_2(size_t size)
+{
+    int i = 1;
+    while (i < size)
+    {
+        i *= 2;
+    }
+    return i;
+}
+int search_for_list_index(size_t size)
+{
+    while ()
+}
+/*Implementação das funções principais*/
+void *heap_initialize()
+{
+    void *new_heap = (1 < MAX_SIZE);
 }
