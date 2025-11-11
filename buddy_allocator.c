b01682765fd6f37180e762d2ea23c88bad04a406
lucasamorimcruz-a11y <lucas.amorim.cruz@gmail.com>
2025-11-11 16:18:40 -0300

    Add files via upload

--- buddy_allocator.c
+++ buddy_allocator.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 #include <sys/mman.h>
 #define MIN_SIZE 6
 #define MAX_SIZE 20
 #define NUMBER_OF_SIZES (MAX_SIZE - MIN_SIZE + 1)
 
-/*Globais e structs*/
 typedef struct buddy_block
 {
     size_t size;
@@ -18,14 +18,11 @@ size_t heap_size = (1 << MAX_SIZE);
 buddy_block *free_lists[NUMBER_OF_SIZES];
 void *heap_start = NULL;
 
-/*Funções auxiliares que serão utilizadas ao longo do código.*/
 bool is_power_of_2(size_t size);
 size_t round_up_to_power_of_2(size_t size);
 int search_for_list_index(size_t size);
 void *buddy_adress_calculation(void *ptr, int k);
-/*Funções principais do alocador
-buddy_split é fundamental pro algoritmo de alocação e buddy_merge é fundamental pro algortimo de liberação.
-*/
+
 void *heap_initialize();
 void *buddy_split(size_t size);
 void *buddy_merge(void *ptr);
@@ -54,10 +51,9 @@ int main()
     buddy_free(ptr);
     buddy_free(second_ptr);
 }
-/*Implementação das funções auxiliares*/
 bool is_power_of_2(size_t size)
 {
-    return ((size & (size - 1)) == 0); // exemplo de como funciona: 8 -> 1000. 7 -> 0111. 0111 & 1000 -> 0000 -> 0, logo, verdadeiro
+    return ((size & (size - 1)) == 0);
 }
 size_t round_up_to_power_of_2(size_t size)
 {
@@ -71,16 +67,16 @@ size_t round_up_to_power_of_2(size_t size)
 int search_for_list_index(size_t size)
 {
     int i = 0;
-    while (free_lists[i]->size != size)
+    while (i < NUMBER_OF_SIZES && ((1 << NUMBER_OF_SIZES) < size))
     {
         i++;
     }
-
+    return i;
 }
-/*Implementação das funções principais*/
+void *find_free_block();
 void *heap_initialize()
 {
-    void *heap_start = mmap(
+    heap_start = mmap(
         NULL,
         heap_size,
         PROT_READ | PROT_WRITE,
@@ -89,23 +85,23 @@ void *heap_initialize()
         0);
     if (heap_start == MAP_FAILED)
     {
-        perror("Erro ao inicializar a heap.");
+        perror("Erro ao inicializar a heap (Sem memória suficiente).");
         return;
     }
     for (int i = 0; i < MAX_SIZE; i++)
     {
-        *free_lists[i] = NULL;
+        free_lists[i] = NULL;
     }
+    
 }
 void *buddy_split(size_t size)
 {
-    int i = search_for_list_index(size);
+        int i = search_for_list_index(size);
     if (free_lists[i] != NULL)
     {
         buddy_block *new_block = free_lists[i];
         free_lists[i] = new_block->next;
         new_block->is_free = false;
-        new_block->size = size;
     }
 }
 void *buddy_merge(void *ptr)
@@ -117,6 +113,10 @@ void *buddy_alloc(size_t size)
     {
         heap_initialize();
     }
+    if (!is_power_of_2(size))
+    {
+        size = round_up_to_power_of_2(size);
+    }
     buddy_block *new_block;
     if (new_block->size)
     {
@@ -127,6 +127,21 @@ void *buddy_calloc(size_t number_of_elements, size_t size)
 }
 void *buddy_realloc(void *ptr, size_t size)
 {
+    if (ptr == NULL)
+    {
+        return buddy_alloc(size);
+    }
+    if (size == 0)
+    {
+        buddy_free(ptr);
+        return NULL;
+    }
+    if (!is_power_of_2(size))
+    {
+        size = round_up_to_power_of_2(size);
+    }
+    void *ptr_to_location = buddy_alloc(size);
+    memmove(ptr_to_location, ptr, 0/*arrumar */);
 }
 void *buddy_free(void *ptr)
 {
